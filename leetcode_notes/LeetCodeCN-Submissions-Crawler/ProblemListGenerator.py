7f5223fed95b891331d80e894d2e7766a48a374d
robinali34 <rli@uei.com>
2020-02-03 12:44:59 -0800

    Add python script by JiayangWu from https://github.com/JiayangWu/LeetCodeCN-Submissions-Crawler

---
+++ leetcode_notes/LeetCodeCN-Submissions-Crawler/ProblemListGenerator.py
@@ -0,0 +1,13 @@
+# Original source from:  https://github.com/JiayangWu/LeetCodeCN-Submissions-Crawler
+# 对应的读取文件格式请参考同文件夹下的 1.txt 文件
+dic = {}
+with open("1.txt", "r", encoding='UTF-8') as f:
+    ff = f.readlines()
+    print(ff)
+    for line in ff:
+        
+        print (line.split("\t"))
+        num, title = line.split("\t")
+        title = title[:-1]
+        dic[title] = int(num)
+print (dic)
