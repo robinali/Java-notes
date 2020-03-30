7f71b5cd0e5e311b13ee06d5b472db6b983d4755
robinali34 <rli@uei.com>
2020-03-29 21:58:59 -0700

    Add DP solutions

--- leetcode_notes/leetcode_cn/0790.多米诺和托米诺平铺/0790-多米诺和托米诺平铺.java
+++ leetcode_notes/leetcode_cn/0790.多米诺和托米诺平铺/0790-多米诺和托米诺平铺.java
@@ -1,12 +1,57 @@
 class Solution {
-    // 790
-    // Reference: lee215 form leetcode
-    public int numTilings(int N) {
-        int a = 0, b = 1, c= 1, c2, mod = 1000000007;
-        while( --N > 0) {
-            c2 = (c * 2 % mod + a) % mod;
-            a = b; b = c; c = c2;
+    /**
+    Base Case: dp[0][0]= 1, dp[0][1] = 0, dp[0][2] = 0
+    Induction Rule
+    dp[i][0] : ways to tile i cols while perfect fill
+    dp[i][1] : ways to tile i cols while missing 1 in 1st row
+    dp[i][2] : ways to tile i cols while missing 1 in 2nd row
+    dp[i][0] = dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]
+    dp[i][1] = dp[i - 1][2]
+    dp[i][2] = dp[i - 1][1]
+    Time complexity: O(n)
+    Space complexity: O(n) -> O(1)
+    */
+    public int numTilings0(int N) {
+        long[][] dp = new long[N+1][3];
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[0][2] = 0;
+        int MOD = 1000000007;
+        for(int i = 1 ; i <= N ; ++i){
+            long temp = i < 2 ? 0 : dp[i -2][0];
+            dp[i][0] = (temp + dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % MOD;
+            dp[i][1] = (temp +dp[i-1][2]) % MOD;
+            dp[i][2] = (temp +dp[i-1][1]) % MOD;
         }
-        return c;
+        return (int)dp[N][0];
+
     }
+
+    /**
+    Base Case: dp[0][0]= 1, dp[0][1] = 0, dp[0][2] = 0
+    Simplified Induction Rule
+    Note: dp[i][1] is always equals to dp[i][2]
+    dp[i][0] = dp[i - 1][0] + dp[i - 2][0]  + 2(dp[i - 1][1])
+    dp[i][1] = dp[i - 2][0] + dp[i - 1][1]
+    Time complexity: O(n)
+    Space complexity: O(n) -> O(1)
+    */
+    public int numTilings(int N) {
+        long[][] dp = new long[N+1][3];
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[0][2] = 0;
+        int MOD = 1000000007;
+        for(int i = 1 ; i <= N ; ++i){
+            long temp = i < 2 ? 0 : dp[i -2][0];
+            dp[i][0] = (temp + dp[i-1][0] + 2 * dp[i-1][1]) % MOD;
+            dp[i][1] = (temp +dp[i-1][1]) % MOD;
+        }
+        return (int)dp[N][0];
+
+    }
+
+    /**
+        Note: you can reduce space by only cache dp[i - 2][0] dp[i - 1][0], dp[i - 1][1]
+    */
 }
